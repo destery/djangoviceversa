273b85f70dd89482afdc85630437e5ec38a36422
destery <ezeushev@bk.ru>
2022-02-12 15:30:42 +0300

    Commit 2

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import views
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('reversed/', views.reversed, name='reversed'),
 ]
